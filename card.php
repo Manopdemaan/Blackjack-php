72baabdc9ced05317c85937b11dfeb5d3dcba4ad
Manopdemaan <112510226+Manopdemaan@users.noreply.github.com>
2024-07-04 15:07:22 +0200

    card

---
+++ card.php
@@ -0,0 +1,12 @@
+<?php
+
+class Kaart {
+    public string $kleur;
+    public string $waarde;
+
+    public function __construct(string $kleur, string $waarde) {
+        $this->kleur = $kleur;
+        $this->waarde = $waarde;
+    }
+}
+?>
